e76bc7bb13f8bdefb7f4a3eba9431d66be5a1d71
Kirill12a <kirilldrozd12@gmail.com>
2018-12-03 17:16:07 +0300

    q

--- k_drozdov.py
+++ k_drozdov.py
@@ -1,19 +1,6 @@
-def function_a():
-    a = 56
-    b = 65
-    return a + b
-
-
-def function_b():
-    a=56
-    c = 3
-    return a + c
-
-def function_c():
-    z=235
-    y=32
-    return z*y
-
-
-print(function_a())
-print(function_b())
+def scuare(x):
+    print('квадрат числа', x , '=' , x**2)
+scuare(4)
+scuare(3)
+scuare(5)
+scuare(8)
