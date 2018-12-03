a7eb68b8a1e083eae2edbf1ce4826be7fd72c605
123POG123 <45313368+123POG123@users.noreply.github.com>
2018-12-03 19:07:13 +0300

    ree

---
+++ cls.py
@@ -0,0 +1,11 @@
+class Lamp():
+    def __init__(self):
+    def swich_on(self):
+        print('lamp on')
+    def swich_off(self):
+        print('lamp off')
+
+Lamp1 = Lamp()
+Lamp2 = Lamp()
+Lamp1.swich_on()
+Lamp1.swich_off()
