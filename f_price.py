687745a77221c38b1010cc2c565fe9ad0937a051
ovr1 <rybynok@mail.ru>
2018-12-05 12:02:47 +0300

    Test2

--- f_price.py
+++ f_price.py
@@ -1,53 +1,53 @@
-def price(pizza,sise,weight):
+def fprice(pizza,sise,weight) -> object:
     if pizza == 'Маргарита':
         if sise == 26:
             if weight == 'normal':
-                print("price = 220 рублей")
+                return("220 рублей")
             else:
-                print("price = 180 рублей")
+                return("180 рублей")
         elif sise == 32:
             if weight == 'normal':
-                print("price = 230 рублей")
+                return("230 рублей")
             else:
-                print("price = 190 рублей")
+                return("190 рублей")
         else:
             sise == 40
             if weight == 'normal':
-                print("price = 250 рублей")
+                return("250 рублей")
             else:
-                print("price = 210 рублей")
+                return("210 рублей")
     elif pizza == 'Паперони':
         if sise == 26:
             if weight == 'normal':
-                print("price = 220 рублей")
+                return("220 рублей")
             else:
-                print("price = 180 рублей")
+                return("180 рублей")
         elif sise == 32:
             if weight == 'normal':
-                print("price = 230 рублей")
+                return("230 рублей")
             else:
-                print("price = 190 рублей")
+                return("190 рублей")
         else:
             sise == 40
             if weight == 'normal':
-                print("price = 250 рублей")
+                return("250 рублей")
             else:
-                print("price = 220 рублей")
+                return("220 рублей")
     else:
         pizza == 'Гавайская'
         if sise == 26:
             if weight == 'normal':
-                print("price = 230 рублей")
+                return("230 рублей")
             else:
-                print("price = 200 рублей")
+                return("200 рублей")
         elif sise == 32:
             if weight == 'normal':
-                print("price = 250 рублей")
+                return("250 рублей")
             else:
-                print("price = 210 рублей")
+                return("210 рублей")
         else:
             sise == 40
             if weight == 'normal':
-                print("price = 280 рублей")
+                return("280 рублей")
             else:
-                print("price = 230 рублей")
+                return("230 рублей")
--- orybynok_OrderPizza.py
+++ orybynok_OrderPizza.py
@@ -1,16 +1,39 @@
-from f_price import price
+from f_price import fprice
+
+
 class Orders:
-    def __init__(self, pizza, sise, weight):
-        self.pizza = pizza
+    def __init__(self, namepizza, sise, weight):
+        self.namepizza = namepizza
         self.sise = sise
         self.weight = weight
     def price(self,pizza, sise, weight):
         price(pizza, sise, weight,)
 
-pizza = input("What pizza do You want: Маргарита, Паперони, Гавайская? -   ")
-sise = input("What sise do You like: 26, 32, 40 ? - ")
-weight = input("What weight do You like: "+"normal"+" or "+"thin"+" ? - ")
-print(pizza,sise,weight)
-price(pizza,sise,weight)
+pizzas = []
+namepizzas = []
+
+flag = True
+while flag:
+    question = input("Do You want to oder pizza?  (yes / no) -   ")
+    if question == 'yes':
+
+        namepizza = input("What pizza do You want: Маргарита, Паперони, Гавайская? -   ")
+        pizza = dict()
+        sise = input("What sise do You like: 26, 32, 40 ? - ")
+        pizza['sise'] = sise
+        weight = input("What weight do You like: "+"normal"+" or "+"thin"+" ? - ")
+        pizza['weight'] = weight
+        price = fprice(pizza,sise,weight)
+        pizza['price'] = price
+        pizzas.append(pizza)
+        namepizzas.append(namepizza)
+        print(namepizzas)
+        print(namepizza,' = ',pizza)
+        print(pizzas)
+    else:
+        flag = False
+        break
+
+
 
 
