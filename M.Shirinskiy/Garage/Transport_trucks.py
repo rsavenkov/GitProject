432793184f78a5139b6ebd073c2bb1594810adb7
Qrowe <mikleshirinskiy@gmail.com>
2018-12-10 19:43:31 +0300

    Added garage list.

--- M.Shirinskiy/Garage/Transport_trucks.py
+++ M.Shirinskiy/Garage/Transport_trucks.py
@@ -27,19 +27,26 @@ class VolvoFE(Truck):
 
     def fullInfo(self):
         info = super().fullInfo()
-        return info + 'Color = {}\nDistance = {}\nPrice = {}\nTransmission = \nOwner = {}\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'\
+        return info + 'Color = {}\nDistance = {}\nPrice = {}\nTransmission = {}\nOwner = {}\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'\
             .format(self.color, self.distance, self.price, self.transmission, self.owner)
+
+garage = []
 
 truck1 = VolvoFMX('lime', 0, 500000*79, '12-gear mechanical transmission', None)
 print('Truck-1: ' + truck1.fullInfo())
+garage.append(truck1)
 
 truck2 = VolvoFMX('red', 0, 450000*79, '10-gear mechanical transmission', None)
 print('Truck-2: ' + truck2.fullInfo())
+garage.append(truck2)
 
 truck3 = VolvoFE('blue', 500000, 100000*79, '9-gear mechanical transmission', 'IE "Frolov"')
 print('Truck-3: ' + truck3.fullInfo())
+garage.append(truck3)
 
-truck4 = VolvoFE('gray', 700000, 50000*79, '9-gear mechanical transmission', 'IE Soloviev')
+truck4 = VolvoFE('gray', 700000, 50000*79, '9-gear mechanical transmission', 'IE "Soloviev"')
 truck4.setWorkable(False)
 print('Truck-4: ' + truck4.fullInfo())
+garage.append(truck4)
 
+print('And thus there are', len(garage), 'trucks.')
