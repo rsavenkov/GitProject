a17a011fb946074e0241eb5adf5a25ed775d99bc
Qrowe <mikleshirinskiy@gmail.com>
2018-12-10 17:27:00 +0300

    Added more trucks. Hope you like Volvo.

--- M.Shirinskiy/Garage/Transport_trucks.py
+++ M.Shirinskiy/Garage/Transport_trucks.py
@@ -15,6 +15,20 @@ class VolvoFMX(Truck):
         return info + 'Color = {}\nDistance = {}\nPrice = {}\nTransmission = {}\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'\
             .format(self.color, self.distance, self.price, self.transmission)
 
+class VolvoFE(Truck):
+
+    def __init__(self, color, distance, price, transmission, owner):
+        super().__init__('Volvo', 'FE', 'cargo trunk truck')
+        self.color = color
+        self.distance = distance
+        self.price = price
+        self.transmission = transmission
+        self.owner = owner
+
+    def fullInfo(self):
+        info = super().fullInfo()
+        return info + 'Color = {}\nDistance = {}\nPrice = {}\nTransmission = \nOwner = {}\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'\
+            .format(self.color, self.distance, self.price, self.transmission, self.owner)
 
 truck1 = VolvoFMX('lime', 0, 500000*79, '12-gear mechanical transmission', None)
 print('Truck-1: ' + truck1.fullInfo())
@@ -22,4 +36,10 @@ print('Truck-1: ' + truck1.fullInfo())
 truck2 = VolvoFMX('red', 0, 450000*79, '10-gear mechanical transmission', None)
 print('Truck-2: ' + truck2.fullInfo())
 
-truck1.setBody('cargo truck')
+truck3 = VolvoFE('blue', 500000, 100000*79, '9-gear mechanical transmission', 'IE "Frolov"')
+print('Truck-3: ' + truck3.fullInfo())
+
+truck4 = VolvoFE('gray', 700000, 50000*79, '9-gear mechanical transmission', 'IE Soloviev')
+truck4.setWorkable(False)
+print('Truck-4: ' + truck4.fullInfo())
+
