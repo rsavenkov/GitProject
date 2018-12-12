c7881147ae58eef9b63fdcc437fe5409a683a207
rsavenkov <lubajnka@mail.ru>
2018-12-12 19:37:36 +0300

    Джамила попросила

--- Inheritance.py
+++ Inheritance.py
@@ -1,4 +1,42 @@
 from car import Car
+from truck import Truck
+from garage import Garage
+
+class Owner:
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+class VolvoFMX(Truck):
+
+    def __init__(self, color, distance, price, transmission, owner):
+        super().__init__('Volvo', 'FMX', 'cargo truck')
+        self.color = color
+        self.distance = distance
+        self.price = price
+        self.transmission = transmission
+        self.owner = owner
+
+    def fullInfo(self):
+        info = super().fullInfo()
+        return info + 'Color = {}\nDistance = {}\nPrice = {}\nTransmission = {}\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'\
+            .format(self.color, self.distance, self.price, self.transmission)
+
+class VolvoFE(Truck):
+
+    def __init__(self, color, distance, price, transmission, owner):
+        super().__init__('Volvo', 'FE', 'cargo trunk truck')
+        self.color = color
+        self.distance = distance
+        self.price = price
+        self.transmission = transmission
+        self.owner = owner
+
+    def fullInfo(self):
+        info = super().fullInfo()
+        return info + 'Color = {}\nDistance = {}\nPrice = {}\nTransmission = {}\nOwner = {}\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'\
+            .format(self.color, self.distance, self.price, self.transmission, self.owner)
 
 class AudiA5(Car):
 
@@ -22,3 +60,13 @@ print('Auto-2: ' + auto2.fullInfo())
 auto1.setBody('cabriolet')
 auto1.setWorkable(False)
 print('Auto-1: ' + auto1.fullInfo())
+
+owner1 = Owner('Misha', 'Petrov')
+truck1 = VolvoFE('white', 10000, 2000000, 'auto', owner1)
+
+owner2 = Owner('Petya', 'Ivanov')
+truck2 = VolvoFMX('black', 20000, 1000000, 'auto', owner2)
+
+garage = Garage('10', 5, [], [])
+
+
