31272580a8a74d762ddb58f0845949adc406e272
rsavenkov <lubajnka@mail.ru>
2018-12-12 19:55:02 +0300

    Гараж для отстающих

--- garage.py
+++ garage.py
@@ -1,3 +1,16 @@
+from truck import Truck
+
+class NotMachineLikeTruckAcceptableInGarage:
+
+    text = '''
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        !!! Only truck acceptable to insert in garage  {}   !!!!!!
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            '''
+
+    def __init__(self, message):
+        self.message = NotMachineLikeTruckAcceptableInGarage.text.format(message)
+
 class Garage:
 
     def __init__(self, number, volume, list1, list2):
@@ -5,3 +18,10 @@ class Garage:
         self.volume = volume
         self.cars_list = list1
         self.trucks_list = list2
+
+    def setTrucks(self, trucks):
+        for truck in trucks:
+            if (isinstance(truck, Truck)):
+                self.trucks_list.append(truck)
+            else:
+                raise NotMachineLikeTruckAcceptableInGarage(truck.type)
