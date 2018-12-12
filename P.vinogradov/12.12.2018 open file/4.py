c8faf62b8969a76b3c6df24750f354653023550c
Pavel <vinogradovpavel32@gmail.com>
2018-12-12 21:56:05 +0300

    new

--- P.vinogradov/12.12.2018 open file/4.py
+++ P.vinogradov/12.12.2018 open file/4.py
@@ -1,13 +1,13 @@
-file_name = 'C:/Users/Ruslan/PycharmProjects/GitProject/trunk/rsavenkov/io operations/новый файл.txt'
+file_name = 'C:/Users/vinog/GitProject1/P.vinogradov/12.12.2018 open file/file'
 
 with open(file_name, 'w', encoding="utf-8") as f:
-    f.write('Это первая строка которую я записал в файл из программы\n')
-    f.write('Это вторая строка которую я записал в файл из программы\n')
-    f.write('Конец\n')
+    f.write('Это первая строка которую я записал в файл из программы/n')
+    f.write('Это вторая строка которую я записал в файл из программы/n')
+    f.write('Конец/n')
 
 with open(file_name, 'r', encoding='utf-8') as f:
     content = f.readlines()
-    # content = f.read()
+     #content = f.read()
 
 for line in content:
     print(line)
