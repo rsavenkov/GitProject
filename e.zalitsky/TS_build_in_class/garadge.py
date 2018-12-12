de2514fb56f3673e09f63f7d11f5350c8169bea7
fuerfenf <egor602@mail.ru>
2018-12-12 10:16:42 +0300

    # garage

--- e.zalitsky/TS_build_in_class/garadge.py
+++ e.zalitsky/TS_build_in_class/garadge.py
@@ -1,4 +1,14 @@
-from KTM import KTM, moto_ktm
-from type_bass import Street, School, garage_bass
-from type_truck import truks, garage_truks
+import KTM
+import type_bass
+import type_truck
+
+
+a = len(KTM.moto_ktm)
+b = len(type_bass.garage_bass)
+c = len(type_truck.garage_truks)
+print("В гараже находится : ", a+b+c, "транспортных средств")
+print("Мотоциклов : ", a)
+print("Автобусов : ", b)
+print("Траков : ", c)
+
 
