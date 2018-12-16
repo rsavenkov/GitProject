20f8bdc084a88b2d3147ec2c14bf8e49b2007a99
Qrowe <mikleshirinskiy@gmail.com>
2018-12-16 18:07:44 +0300

    New name for a directory.

---
+++ mshirinskiy/Garage/Garage.py
@@ -0,0 +1,72 @@
+from truck import Truck
+from Transport_trucks import VolvoFMX
+from Transport_trucks import VolvoFE
+
+class NotMachineLikeTruckAcceptableInGarage(Exception):
+
+    text = '''
+           !!!!!!!!!!!!!!!!!!!!!!
+           !!!!!! Only trucks are acceptable to be parked in this garage {} !!!!!!!!!!!!
+           !!!!!!!!!!!!!!!!!!!!!!
+    '''
+
+    def __init__(self, message):
+        self.message = NotMachineLikeTruckAcceptableInGarage.text.format(message)
+
+class NotVolvoFMXInBox(NotMachineLikeTruckAcceptableInGarage):
+
+    text = '''!!!!!!!!!!!!!!!!!!!!!!!
+           !!!!!!!!!!!!   One or more trucks you have parked are not VolvoFMX model !!!!!!!!!!!!!
+           !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+           '''
+
+    def __init__(self, message):
+        self.message = NotVolvoFMXInBox.text.format(message)
+
+class NotVolvoFEInBox(NotMachineLikeTruckAcceptableInGarage):
+
+    text = '''!!!!!!!!!!!!!!!!!!!!!!!
+           !!!!!!!!!!!!   One or more trucks you have parked are not VolvoFE model !!!!!!!!!!!!!
+           !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+           '''
+
+    def __init__(self, message):
+        self.message = NotVolvoFEInBox.text.format(message)
+
+class Garage:
+
+    def __init__(self, number, volume, list1, list2):
+        self.number = number
+        self.volume = volume
+        self.cars_list = list1
+        self.trucks_list = list2
+
+    def setTrucks(self, trucks):
+        for truck in trucks:
+            if (isinstance(truck, Truck)):#Если у нас грузовик, то мы добавляем его в список
+                self.trucks_list.append(truck)
+            else:
+                raise NotMachineLikeTruckAcceptableInGarage(truck.type)
+
+class Garage_Box(Garage):
+
+    def __init__(self, number, list1):
+        self.number = number
+        self.trucks_list = list1
+
+    def setVolvoFMXBox(self, trucks):
+        for truck in trucks:
+            if(isinstance(Truck.model, FMX)):
+                self.trucks_list.append(truck)
+            else:
+                raise NotVolvoFMXInBox(truck.model)
+
+    def setVolvoFEBox(self, trucks):
+        for truck in trucks:
+            if(isinstance(Truck.model, FE)):
+                self.trucks_list.append(truck)
+            else:
+                raise NotVolvoFEInBox(truck.model)
+
+
+
---
+++ mshirinskiy/Garage/Inheritance.py
@@ -0,0 +1,36 @@
+from car import Car
+from Garage import Garage, NotMachineLikeTruckAcceptableInGarage
+
+class AudiA5(Car):
+
+    def __init__(self, color, distance, price, owner):
+        super().__init__('audi', 'A5', 'sedan')
+        self.color = color
+        self.distance = distance
+        self.price = price
+        self.owner = owner
+
+    def fullInfo(self):
+        info = super().fullInfo()
+        return info + 'Color = {}\nDistance = {}\nPrice = {}\n-----------------'.format(self.color, self.distance, self.price)
+
+garage=[]
+
+auto1 = AudiA5('white', 0, 500 * 1000, None)
+print('Auto-1: ' + auto1.fullInfo())
+garage.append(auto1)
+
+auto2 = AudiA5('black', 10 * 1000, 350 * 1000, None)
+print('Auto-2: ' + auto2.fullInfo())
+garage.append(auto2)
+
+auto1.setBody('cabriolet')
+auto1.setWorkable(False)
+print('Auto-1: ' + auto1.fullInfo())
+print('And thus there are', len(garage), 'cars in the garage.')
+
+garage = Garage('8', 500, [], [])
+try:
+    garage.setTrucks([auto1, auto2])
+except NotMachineLikeTruckAcceptableInGarage as e:
+    print(e.message)
