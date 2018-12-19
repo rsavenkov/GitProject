48d15298fef91fcb85a93c821f694574a4420f31
rsavenkov <lubajnka@mail.ru>
2018-12-19 16:56:47 +0300

    Улучшенная версия консольной программы

--- rsavenkov/console program.py
+++ rsavenkov/console program.py
@@ -1,6 +1,15 @@
 import os
 
+class QuitProgramException(Exception):
+    '''
+    My program quit exception
+    '''
+    def __init__(self):
+        self.message = 'Сеанс ввода завершен. Посмотрите содержимое файла {}'
+
 disk_letters = ['A', 'B', 'C', 'D']
+
+print('Добро пожаловать в консольную программу записи файла! Для выхода из нее введите команду quit()')
 
 file = input('Введите имя файла:')
 if (file[0] not in disk_letters) or (file[1:3] != ':\\'):
@@ -15,21 +24,29 @@ for dir in dirs:
     path = path + '\\' + dir
     if not os.path.isdir(file[0:3] + path):
         print('Введенное значение не является файлом!')
-        exit(0)
+        exit(-1)
 
 #os.path.isfile()
 
-# доработать программу так чтобы последняя строчка сохранялась когда на ней нажимаю ctrl + z
+error_code = 0
 first = input('Введите содержимое файла:')
 with open(file, 'w', encoding='utf-8') as f:
     try:
         f.writelines(first + '\n')
         while True:
             line = input()
+            if line == 'quit()':
+                raise QuitProgramException()
             f.writelines(line + '\n')
+    except QuitProgramException as e:
+        print(e.message.format(f.name))
+        error_code = 0
     except KeyboardInterrupt as e:
-        print('Сеанс ввода завершен. Посмотрите содержимое файла {}'.format(f.name))
-        exit(0)
+        print('Непредвиденное завершение программы! Фалй не создан!')
+        error_code = 2
 
-exit(0)
+if error_code != 0:
+    os.remove(file)
+
+exit(error_code)
 
