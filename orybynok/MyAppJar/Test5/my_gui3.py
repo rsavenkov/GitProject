a00a2a3eb0b7826dfc9001f5196b1d8e82c66005
ovr1 <rybynok@mail.ru>
2018-12-27 20:59:47 +0300

    File work

---
+++ orybynok/MyAppJar/Test5/my_gui3.py
@@ -0,0 +1,69 @@
+from typing import List, Any
+from appJar import gui
+import json
+file_name = 'registration3.txt'
+number = 'number3.txt'
+
+testn = []
+# handle button events
+
+def press(button):
+    if button == 'Очистить':
+        app.stop()
+    else:
+        usr = app.getEntry("Фамилия")
+        testn.append(usr)
+        name = app.getEntry("Имя")
+        testn.append(name)
+        app.getLabelOptionBox("- Ваш Возраст -",["18-25","26-35","36-45","46-55+"], press)
+        app.getLabelOptionBox("- Цель Визита -", ["Консультация",
+                                     "Классический массаж", "СПА", "Медитация"], press)
+        pwd = app.getAllOptionBoxes()
+        testn.append(pwd)
+        inf = app.getTextArea("Дополнительные сведения о себе")
+        testn.append(inf)
+
+
+        with open(file_name, 'a', encoding="utf-8") as f:
+            f.write(str(testn)+'\n')
+
+# content = f.read()
+
+        with open(file_name, 'r', encoding='utf-8') as f:
+            contents = f.read()
+            x = contents.splitlines()
+            y = len(x)
+            print(contents)
+            print(str(y))
+
+        with open(number, 'w', encoding="utf-8") as f:
+            f.write(str(y))
+
+
+
+# create a GUI variable called app
+app = gui("Login Window", "400x400")
+app.setBg("yellow")
+app.setFont(18)
+
+# add & configure widgets - widgets get a name, to help referencing them later
+app.addLabel("title", "Welcome to appJar")
+app.setLabelBg("title", "blue")
+app.setLabelFg("title", "yellow")
+
+app.addLabelEntry("Фамилия")
+app.addLabelEntry("Имя")
+app.addLabelOptionBox("- Ваш Возраст -",["18-25","26-35","36-45","46-55+"])
+app.addLabelOptionBox("- Цель Визита -", ["Консультация",
+                                     "Классический массаж", "СПА", "Медитация"])
+app.addScrolledTextArea("Дополнительные сведения о себе")
+# link the buttons to the function called press
+
+
+app.addButtons(["Ввод", 'Очистить',], press)
+
+app.setFocus("Фамилия")
+
+
+# start the GUI
+app.go()
