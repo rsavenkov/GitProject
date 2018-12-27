12e09a00c4d1d6c6a7726694f5d67227e089d3d3
ovr1 <rybynok@mail.ru>
2018-12-27 09:40:17 +0300

    File work

--- orybynok/MyAppJar/Test5/file_read.py
+++ orybynok/MyAppJar/Test5/file_read.py
@@ -1,5 +1,13 @@
 file_name = 'registration.txt'
 
+
 with open(file_name, 'r', encoding='utf-8') as f:
     contents = f.read()
     print(contents)
+
+
+    if 'Test1'and'Oleg'and'0209' in contents:
+        print('True')
+    else:
+        print('False')
+
