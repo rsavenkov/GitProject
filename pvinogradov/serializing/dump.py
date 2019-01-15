831cfc347d85d43fa13d1893567d32f9d74f4532
123POG123 <45313368+123POG123@users.noreply.github.com>
2019-01-15 19:59:52 +0300

    11

--- pvinogradov/serializing/dump.py
+++ pvinogradov/serializing/dump.py
@@ -3,4 +3,4 @@ import pickle
 colors = {1: 'yellow', 2: 'red', 3: 'black', 4: 'green'}
 
 with open('C:\Users\Oleg\Documents\GitHub\GitProject\rsavenkov\serializing\dump.py\colors.pickle', 'wb') as f:
-    pickle.dump(colors, f)
+    pickle.dump(colors, f )
