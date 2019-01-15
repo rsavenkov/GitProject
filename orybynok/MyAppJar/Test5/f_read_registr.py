a993caa36af64aa97ead98314ef38bd6a46cfe26
ovr1 <rybynok@mail.ru>
2019-01-15 22:39:28 +0300

    finish

--- orybynok/MyAppJar/Test5/f_read_registr.py
+++ orybynok/MyAppJar/Test5/f_read_registr.py
@@ -1,4 +1,6 @@
 file_name = 'registration3.txt'
+file_name1 = 'number3.txt'
+import pickle
 
 print("Список пациентов: \n")
 with open(file_name, 'r', encoding='utf-8') as f:
@@ -13,4 +15,11 @@ with open(file_name, 'r', encoding="utf-8") as f:
     cont = f.readlines()
     Kol_pacientov: int = len(cont)
     print('Kol_pacientov = ', Kol_pacientov)
+
+    with open('Kol_pacientov.pickle','wb') as f:
+        pickle.dump(Kol_pacientov, f)
+
+    with open('Kol_pacientov.pickle','rb') as f:
+        print('Kol_pacientov = ', pickle.load)
+
 
