2b16b70795f241f5507b8eac4be5b40214b14731
ovr1 <rybynok@mail.ru>
2019-01-16 13:17:44 +0300

    finish

--- orybynok/MyAppJar/Test5/f_read_zapisi.py
+++ orybynok/MyAppJar/Test5/f_read_zapisi.py
@@ -7,6 +7,8 @@ with open(file_name1, 'r', encoding='utf-8') as f:
     for line in contents:
         ds = eval(str(line[1:-2]))
         S.append(ds['Пациент'])
+    n = [1,2,3,...,100]
+    S = list(zip(n,S))
 l = []
 L = []
 with open(file_name, 'r', encoding='utf-8') as f:
