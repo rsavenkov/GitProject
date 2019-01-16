e8acf3d564cc4f5054f0bbb14a40020d0cead1ff
Rusklim24 <rusklim22@gmail.com>
2019-01-16 20:51:22 +0300

    16.01.19

---
+++ rklimenko/r_klimenko_pickle.py
@@ -0,0 +1,26 @@
+import pickle
+import os
+
+'''
+Поменяйте путь к файлу
+'''
+
+a = True
+
+l = []
+
+with open('C:/Users/Ruslan/PycharmProjects/GitProject/rklimenko/example.py', 'wb') as f:
+    c = pickle.dump(a,f)
+
+with open('C:/Users/Ruslan/PycharmProjects/GitProject/rklimenko/example.py', 'rb') as f:
+    b = pickle.load(f)
+    print(b)
+
+c = int(input('Do you want to delete the file? Print "1" if yes, print "0" if no: '))
+l.append(c)
+
+d = l[0]
+if d == 1:
+    os.remove('example.py')
+else:
+    print('Well, well...')
