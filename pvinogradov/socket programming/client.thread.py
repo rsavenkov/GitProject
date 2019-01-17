bd385d05114f6a7b4be7dbcecbab59710aca24d5
123POG123 <45313368+123POG123@users.noreply.github.com>
2019-01-17 17:00:56 +0300

    SOCKET

---
+++ pvinogradov/socket programming/client.thread.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+import sys
+
+host = "192.168.0.100"
+port = 1080
+sock = socket.socket()
+sock.connect((host, port))
+
+def Reciver():
+    while 1:
+        data = sock.recv(1024)
+        if data:
+            print(data.decode())
+
+def Sender():
+    while 1:
+        message = input()
+        if message == "exit":
+            sock.close()
+            sys.exit()
+        else:
+            sock.send(message.encode())
+
+# init threads
+t1 = threading.Thread(target=Reciver)
+t2 = threading.Thread(target=Sender)
+
+# start threads
+t1.start()
+t2.start()
