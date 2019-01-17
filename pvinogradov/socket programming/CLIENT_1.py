a1e8f0e1d20ed04f9c7647e6e3fd0cc1e3a331c9
123POG123 <45313368+123POG123@users.noreply.github.com>
2019-01-17 16:59:50 +0300

    SOCKET

--- pvinogradov/socket programming/CLIENT_1.py
+++ pvinogradov/socket programming/CLIENT_1.py
@@ -2,7 +2,8 @@ import socket
 
 sock = socket.socket()
 sock.connect(('192.168.43.164', 9090))
-sock.send('hello, world!')
+a = ('hello, world!')
+sock.send(a.encode())
 
 data = sock.recv(1024)
 sock.close()
---
+++ pvinogradov/socket programming/server.thread.py
@@ -0,0 +1,50 @@
+import socket
+import threading  # многопоточность
+import sys
+
+sock = socket.socket()
+sock.bind(('192.168.0.100', 1080))
+sock.listen(3)
+conn = []
+
+
+def Reciver():
+    while 1:
+        for i in range(len(conn)):
+            try:
+                data = conn[i].recv(1024)
+                if data:
+                    print(data.decode())
+            except socket.error as e:
+                if e.errno == 10053:
+                    conn.pop(i)
+                    print("Подключено пользователй:", len(conn))
+                else:
+                    raise
+
+
+def Sender():
+    while 1:
+        global conn
+        message = input()
+        if message:
+            for i in range(len(conn)):
+                conn[i].send(message.encode())
+
+
+def Accepter():
+    while 1:
+        global conn
+        conn.append(sock.accept()[0])
+        print("Подключено пользователй:", len(conn))
+
+
+# подключаем потоки передаем в параметры наши функции
+t1 = threading.Thread(target=Reciver)
+t2 = threading.Thread(target=Sender)
+t3 = threading.Thread(target=Accepter)
+
+# даем старт потокам
+t1.start()
+t2.start()
+t3.start()
