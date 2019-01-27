2d22f25d9982714d523d0fe4e90ae17effefa1d1
fuerfenf <egor602@mail.ru>
2019-01-27 12:08:06 +0300

    #

---
+++ ezalitsky/different/datebase/lessons/table_in_file.py
@@ -0,0 +1,12 @@
+import postgresql
+
+db = postgresql.open('pq://egor:123@127.0.0.1:5432/mytestdb')
+get_emp_with_salary_lt = db.prepare("SELECT * FROM students")
+
+with db.xact():
+    with open(
+            r'C:\Users\Xiaomi\Documents\Exploit_directories\Python\PycharmProjects\GitProject\ezalitsky\different\datebase\my_db_table',
+            "w+", encoding="utf-8") as f:
+        for row in get_emp_with_salary_lt:
+                f.writelines(str(row))
+                f.writelines('\n')
