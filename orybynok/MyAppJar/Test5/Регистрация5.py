5e9c0414c98c1793a675347709e4cacf1fae4181
ovr1 <rybynok@mail.ru>
2019-01-31 21:38:48 +0300

    Tet2

--- orybynok/MyAppJar/Test5/Регистрация5.py
+++ orybynok/MyAppJar/Test5/Регистрация5.py
@@ -57,7 +57,6 @@ def press(button):
                     V.insert(0, N)
                     L = tuple(V)
                     print(L)
-
                     with db.xact():
                         tablReg(L[0], str(L[1]), L[2], str(L[3]), L[4], str(L[5]),str(L[6]))
 
