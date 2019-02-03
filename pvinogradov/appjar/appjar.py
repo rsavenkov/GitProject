661c1e93bdd0a38eb7ad204f4057ac3079bb23b0
123POG123 <45313368+123POG123@users.noreply.github.com>
2019-02-03 09:48:08 +0300

    App1

--- pvinogradov/appjar/appjar.py
+++ pvinogradov/appjar/appjar.py
@@ -1,89 +1,91 @@
+from tkinter import *  #
+from tkinter import messagebox  #
+from tkinter import ttk  #
+
 import postgresql
 from appJar import gui
-from tkinter import * #
-from tkinter import messagebox #
-from tkinter import ttk #
 
-app = gui( 'Add datebase', '500x300' )
+app = gui( 'Add datebase', '710x300' )
 
 db = postgresql.open( "pq://postgres:123@127.0.0.1:5432/paveldb" )
 
 
+def menuPress(btn):
+    if btn == 'Calculator':
+        root = Tk()
+        root.title( 'Calculiator' )
 
+        def calc(key):
+            if key == "=":
+                str1 = "//%-+1234567890./*"
+                if calc_entry.get()[0] not in str1:
+                    calc_entry.insert( END, " The first character is not a number" )
+                    messagebox.showerror( "Mistake!!! you entered not a number" )
 
-def menuPress():
-    root = Tk()
-    root.title( 'Calculiator' )
+                try:
+                    result = eval( calc_entry.get() )
+                    calc_entry.delete( 0, END )
+                    calc_entry.insert( END, str( result ) )
+                except:
+                    calc_entry.insert( END, " Mistake!" )
+                    messagebox.showerror( 'Error ', ' check data validity' )
 
-    def calc(key):
-        global memory
-        if key == "=":
-
-            str1 = "//%-+1234567890./*"
-            if calc_entry.get()[0] not in str1:
-                calc_entry.insert( END, " The first character is not a number" )
-                messagebox.showerror( "Mistake!!! you entered not a number" )
-
-            try:
-                result = eval( calc_entry.get() )
+            elif key == "c":
                 calc_entry.delete( 0, END )
-                calc_entry.insert( END, str( result ) )
-            except:
-                calc_entry.insert( END, " Mistake!" )
-                messagebox.showerror( 'Error ',' check data validity' )
 
-        elif key == "c":
-            calc_entry.delete( 0, END )
+            elif key == '-/+':
+                if '=' in calc_entry.get():
+                    calc_entry.delete( 0, END )
+                try:
+                    if calc_entry.get()[0] == '-':
+                        calc_entry.delete( 0 )
+                    else:
+                        calc_entry.insert( 0, '-' )
+                except IndexError:
+                    pass
+            else:
+                if '=' in calc_entry.get():
+                    calc_entry.delete( 0, END )
+                calc_entry.insert( END, key )
 
-        elif key == '-/+':
-            if '=' in calc_entry.get():
-                calc_entry.delete( 0, END )
-            try:
-                if calc_entry.get()[0] == '-':
-                    calc_entry.delete( 0 )
-                else:
-                    calc_entry.insert( 0, '-' )
-            except IndexError:
-                pass
-        else:
-            if '=' in calc_entry.get():
-                calc_entry.delete( 0, END )
-            calc_entry.insert( END, key )
+        bttn_list = [
+            '7', '8', '9', '+', '-',
+            '4', '5', '6', '*', '/',
+            '1', '2', '3', '-/+', '=',
+            '0', '.', 'c', '//', '%',
+        ]
+        r = 1
+        c = 0
+        for i in bttn_list:
+            cmd = lambda x=i: calc( x )
+            ttk.Button( root, text=i, command=cmd ).grid( row=r, column=c, sticky=E )
+            c += 1
 
+            if c > 4:
+                c = 0
+                r += 1
 
+        calc_entry = Entry( root, width=50, borderwidth=5 )
 
-    bttn_list = [
-        '7', '8', '9', '+', '-',
-        '4', '5', '6', '*', '/',
-        '1', '2', '3', '-/+', '=',
-        '0', '.', 'c', '//', '%',
-    ]
-    r = 1
-    c = 0
-    for i in bttn_list:
-        cmd = lambda x=i: calc( x )
-        ttk.Button( root, text=i, command=cmd ).grid( row=r, column=c, sticky=E )
-        c += 1
+        calc_entry.grid( row=0, column=0, columnspan=5 )
+        root.mainloop()
 
-        if c > 4:
-            c = 0
-            r += 1
-
-    calc_entry = Entry( root, width=50, borderwidth=5 )
-
-    calc_entry.grid( row=0, column=0, columnspan=5 )
-    root.mainloop()
 
 fileMenus = ["Open", "Save", "Save as...", "-", "Export", "Print", "-", "Close"]
-app.addMenuList("File", fileMenus, menuPress)
-fileMenus1 = ['Calculator']
-app.addMenuList("Tools", fileMenus1, menuPress)
+app.addMenuList( "File", fileMenus, menuPress )
+fileMenus1 = ['Calculator', 'Paint']
+app.addMenuList( "Tools", fileMenus1, menuPress )
 
 
-def menu():
-    app.setBg("green")
-    app.setFont(18)
-    app.addButtons(["Revision", "Quiz", "Progress"], press)
+# def menu():
+#     app.setBg("green")
+#     app.setFont(18)
+#     app.addButtons(["Revision", "Quiz", "Progress"], press)
+
+
+def press1(btn):
+    if btn == "OFF":
+        app.stop()
 
 
 def press(btn):
@@ -94,7 +96,6 @@ def press(btn):
         name = app.getEntry( 'name1' )
         surname = app.getEntry( 'SurName1' )
         age = int( app.getEntry( 'Age' ) )
-
 
         ochered = db.prepare( '''
 
@@ -114,12 +115,13 @@ def press(btn):
             # a.append( row )
             print( row )
 
-            app.addMessage(row)
+            app.addMessage( row )
 
     elif btn == 'Reset':
-        print( '123' )
-        menu()
-
+        a1.delete( [0], END )
+        a2.delete( [0], END )
+        a3.delete( [0], END )
+        # menu()
 
 
 # this is a pop-up
@@ -133,17 +135,15 @@ tools = ["ABOUT", "REFRESH", "OPEN", "CLOSE", "SAVE",
          "REDO", "PREFERENCES", "HOME", "HELP", "CALENDAR",
          "WEB", "OFF"]
 
-app.addToolbar( tools, press, findIcon=True )
+app.addToolbar( tools, press1, findIcon=True )
 
-app.addEntry( 'name1', column=0, row=0 )
+a1 = app.addEntry( 'name1', column=0, row=0 )
 app.setEntryDefault( "name1", "Name" )
 
-
-
-app.addEntry( 'SurName1', column=0, row=1 )
+a2 = app.addEntry( 'SurName1', column=0, row=1 )
 app.setEntryDefault( "SurName1", "Surname" )
 
-app.addEntry( 'Age', column=0, row=2 )
+a3 = app.addEntry( 'Age', column=0, row=2 )
 app.setEntryDefault( "Age", "Age" )
 
 app.buttons( ["Add database", "Show", "Cancel", "Reset"], press )
