bb353c49e2567d6c8070f13c90ebfb6af1d95e29
Qrowe <mikleshirinskiy@gmail.com>
2019-02-06 19:54:22 +0300

    Count students per teacher

---
+++ mshirinskiy/Select_teachers.py
@@ -0,0 +1,15 @@
+import postgresql
+
+db = postgresql.open("pq://postgres:040692@127.0.0.1:5432/Database")
+
+# select_teachers=db.prepare('''SELECT teachers.first_name, teacher.id, count(students.*)''')
+
+group_by = db.prepare('''
+select  teachers.first_name, teachers.id, count(students.*)
+  from students join teachers on students.teachers_id = teachers.id
+group by teachers.id, teachers.first_name;
+
+''')
+
+for row in group_by:
+    print(row)
