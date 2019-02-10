460ab3b33626ea7e7af43bdd6f443353e6ff8d80
Mailz95 <verwolfik@mail.ru>
2019-02-10 23:04:57 +0300

    Finaly it's happened.

--- ematsyk/WEB/My_cherrypy.py
+++ ematsyk/WEB/My_cherrypy.py
@@ -3,7 +3,7 @@ import cherrypy
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        with open("my_cherryy.py", read, encoding='windows-1251') as f:
+        with open("C:/Users/Mailz/PycharmProjects/GitProject/ematsyk/WEB/My_cherrypy.py", 'r', encoding='windows-1251') as f:
             return f.read()
 
 cherrypy.quickstart(HelloWorld())
