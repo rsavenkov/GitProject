3fa380e4d36bfb470b1ff9664434db68ab96ffca
Mailz95 <verwolfik@mail.ru>
2019-02-10 22:57:47 +0300

    WEB

--- ematsyk/WEB/My_cherrypy.py
+++ ematsyk/WEB/My_cherrypy.py
@@ -3,7 +3,7 @@ import cherrypy
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        with open(my_cherryy.py, read, encoding='windows-1251') as f:
+        with open("my_cherryy.py", read, encoding='windows-1251') as f:
             return f.read()
 
 cherrypy.quickstart(HelloWorld())
